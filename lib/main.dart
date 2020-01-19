913066732c9e23776817376c98e5ca67fd9f1443
Vijay Shukla <vshukla684@gmail.com>
2020-01-19 14:22:48 +0530

    reset master for new code base

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,58 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DicePage(),
-    );
-  }
-}
-
-class DicePage extends StatefulWidget {
-  const DicePage({Key key}) : super(key: key);
-
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 6;
-
-  void changeDiceNumber(){
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Dice'),
-      ),
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                child: Image.asset('images/dice$leftDiceNumber.png'),
-                onPressed: () {
-                  changeDiceNumber();
-                },
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                child: Image.asset('images/dice$rightDiceNumber.png'),
-                onPressed: () {
-                  changeDiceNumber();
-                },
-              ),
-            ),
-          ],
-        ),
+      home: Scaffold(
+        body: Container(),
       ),
     );
   }
